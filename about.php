c7ea3807f6a85035b6138de35f2771a5e0686ad0
Tride <matttride@gmail.com>
2026-05-11 22:42:15 +0800

    新增项目说明页面

---
+++ about.php
@@ -0,0 +1,40 @@
+<?php
+$pageTitle = "项目说明";
+require_once 'includes/header.php';
+?>
+
+<main class="home">
+    <section class="hero">
+        <p class="eyebrow">项目说明</p>
+        <h1>关于Tride的图书管理系统</h1>
+        <p class-="intro">
+            本项目是Web课程的大作业
+        </p>
+    </section>
+
+    <section class="features">
+        <div class="features-card">
+            <h2>项目主题</h2>
+            <p>
+                本网站模拟一个小型图书馆的基本业务流程。
+            </p>
+        </div>
+
+        <div class="features-card>">
+            <h2>主要功能</h2>
+            <p>
+                网站计划实现用户注册登录、图书浏览、图书增删改查、借阅归还、资料上传、
+                借阅记录下载等功能，同时满足对数据库操作的要求。
+            </p>
+        </div>
+
+        <div class="features-card">
+            <h2>安全设计</h2>
+            <p>后续开发中会增加安全措施</p>
+        </div>
+    </section>
+</main>
+
+<?php
+require_once 'index/footer.php';
+?>
